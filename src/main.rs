55be2120339d6c6706ac8f07d0f57cf9a1c44da5
Frazzer951 <luke343279@gmail.com>
2022-12-07 19:14:35 -0800

    add framework for cli commands

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,47 @@
+use clap::{Parser, Subcommand};
+
 pub mod utils;
 mod y2022;
 
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+#[command(propagate_version = true)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Adds files for year and day
+    Add { year: i32, day: i32 },
+}
+
 fn main() {
-    let run_2022 = true;
+    let cli = Cli::parse();
 
-    // 2022
-    if run_2022 {
-        // Day 1
-        y2022::day_1::run();
-
-        // Day 2
-        y2022::day_2::run();
+    if let Some(command) = &cli.command {
+        match command {
+            Commands::Add { year, day } => {
+                println!("Adding files for Year: {year} Day: {day}")
+            },
+        }
+    } else {
+        run_years(Years { y2022: true })
     }
+}
+
+struct Years {
+    y2022: bool,
+}
+
+fn run_years(years: Years) {
+    if years.y2022 {
+        y2022()
+    }
+}
+
+fn y2022() {
+    y2022::day_1::run();
+    y2022::day_2::run();
 }
