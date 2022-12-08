36c5a6dd9ae9b4b14f218bb49f9e4d702fac8c25
Luke Eltiste <luke343279@gmail.com>
2022-12-07 16:28:32 -0800

    2022 Day 2

--- src/main.rs
+++ src/main.rs
@@ -14,5 +14,14 @@ fn main() {
 
         let d1_p2 = y2022::day_1::part_2(d1_input);
         println!("2022 Day 01 - P2 - {d1_p2}");
+
+        // Day 2
+        let d2_input = utils::read_input(2022, 2);
+
+        let d2_p1 = y2022::day_2::part_1(d2_input.clone());
+        println!("2022 Day 02 - P1 - {d2_p1}");
+
+        let d2_p2 = y2022::day_2::part_2(d2_input);
+        println!("2022 Day 02 - P2 - {d2_p2}");
     }
 }
