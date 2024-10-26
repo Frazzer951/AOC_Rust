f669ae51cf50612d07794a320dfdb2b006ad45dc
Luke <luke343279@gmail.com>
2024-10-26 11:02:53 -0700

    2015 day 1

--- src/y2015/day_1.rs
+++ src/y2015/day_1.rs
@@ -4,7 +4,7 @@ pub struct AocDay;
 
 impl Day for AocDay {
     fn run(&self) {
-        let input = crate::utils::read_input(2015, 1);
+        let input = crate::utils::read_input(2015, 1)[0].to_owned();
 
         println!(" Day 01:");
 
@@ -16,31 +16,57 @@ impl Day for AocDay {
     }
 }
 
-fn part_1(input: Vec<String>) -> i32 {
-    0
+fn part_1(input: String) -> i32 {
+    let mut height: i32 = 0;
+
+    for char in input.chars() {
+        match char {
+            '(' => height += 1,
+            ')' => height -= 1,
+            _ => panic!("Invalid Char"),
+        }
+    }
+
+    height
 }
 
-fn part_2(input: Vec<String>) -> i32 {
-    0
+fn part_2(input: String) -> i32 {
+    let mut height: i32 = 0;
+    let mut position: i32 = 0;
+
+    for char in input.chars() {
+        position += 1;
+        match char {
+            '(' => height += 1,
+            ')' => height -= 1,
+            _ => panic!("Invalid Char"),
+        }
+        if height < 0 {
+            return position;
+        }
+    }
+
+    position
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use test_case::test_case;
 
-    fn example_input() -> Vec<String> {
-        [""].iter().map(|s| String::from(*s)).collect::<Vec<_>>()
+    #[test_case(vec!["(())", "()()"], 0 ; "0")]
+    #[test_case(vec!["(((", "(()(()(", "))((((("], 3 ; "3")]
+    #[test_case(vec!["())", "))("], -1 ; "neg1")]
+    #[test_case(vec![")))", ")())())"], -3 ; "neg3")]
+    fn test_p1(inputs: Vec<&str>, expected: i32) {
+        for input in inputs {
+            assert_eq!(part_1(input.to_owned()), expected);
+        }
     }
 
-    #[test]
-    fn test_p1() {
-        let input = example_input();
-        assert_eq!(part_1(input), 1);
-    }
-
-    #[test]
-    fn test_p2() {
-        let input = example_input();
-        assert_eq!(part_2(input), 1);
+    #[test_case(")", 1)]
+    #[test_case("()())", 5)]
+    fn test_p2(input: &str, expected: i32) {
+        assert_eq!(part_2(input.to_owned()), expected);
     }
 }
