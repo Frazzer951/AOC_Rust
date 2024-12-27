75ae0335bc9b8d0187ec67e2ee4479e0a0e47746
Luke <luke343279@gmail.com>
2024-12-26 21:14:01 -0800

    total rewrite

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+macro_rules! library {
+    ($year:tt $description:literal $($day:tt),*) => {
+        #[doc = concat!("# ", $description)]
+        pub mod $year {$(pub mod $day;)*}
+    }
+}
+
+library!(util "Utility modules to handle common recurring Advent of Code patterns."
+    ansi, bitset, grid, hash, heap, integer, iter, math, md5, parse, point, slice, thread
+);
+
+
+library!(year2023 "Restore global snow production."
+    day01
+);
+
--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,88 @@
-use clap::{Parser, Subcommand};
-
-mod advent_of_code;
-mod utils;
-mod y2015;
-mod y2022;
-mod y2023;
-mod years;
-
-#[derive(Parser)]
-#[command(author, version, about, long_about = None)]
-#[command(propagate_version = true)]
-struct Cli {
-    #[command(subcommand)]
-    command: Option<Commands>,
-}
-
-#[derive(Subcommand)]
-enum Commands {
-    /// Adds files for year and day
-    Add { year: u32, day: u32 },
-    /// Run a specific year and day
-    RunDay { year: u32, day: u32 },
-    /// Run a specific year
-    RunYear { year: u32 },
-}
+use aoc::util::ansi::*;
+use aoc::util::parse::*;
+use aoc::*;
+use std::env::args;
+use std::fs::read_to_string;
+use std::iter::empty;
+use std::path::{Path, PathBuf};
+use std::time::{Duration, Instant};
 
 fn main() {
-    let cli = Cli::parse();
-    let aoc = advent_of_code::AdventOfCode::new();
+    // Parse command line options
+    let (year, day) = match args().nth(1) {
+        Some(arg) => {
+            let str = arg.as_str();
+            let mut iter = str.iter_unsigned();
+            (iter.next(), iter.next())
+        }
+        None => (None, None),
+    };
 
-    match &cli.command {
-        Some(Commands::Add { year, day }) => {
-            utils::create_year_day(*year, *day);
+    // Filter solutions
+    let solutions: Vec<_> = empty()
+        .chain(year2023())
+        .filter(|solution| year.is_none_or(|y: u32| y == solution.year))
+        .filter(|solution| day.is_none_or(|d: u32| d == solution.day))
+        .collect();
+
+    // Pretty print output for each solution.
+    let mut duration = Duration::ZERO;
+
+    for Solution { year, day, path, wrapper } in &solutions {
+        if let Ok(data) = read_to_string(path) {
+            let instant = Instant::now();
+            let (part1, part2) = wrapper(data);
+            duration += instant.elapsed();
+
+            println!("{BOLD}{YELLOW}{year} Day {day:02}{RESET}");
+            println!("    Part 1: {part1}");
+            println!("    Part 2: {part2}");
+        } else {
+            eprintln!("{BOLD}{RED}{year} Day {day:02}{RESET}");
+            eprintln!("    Missing input!");
+            eprintln!("    Place input file in {BOLD}{WHITE}{}{RESET}", path.display());
         }
-        Some(Commands::RunDay { year, day }) => {
-            aoc.run_year(
-                *year,
-                &years::Days {
-                    all: false,
-                    days: vec![*day],
-                },
-            );
-        }
-        Some(Commands::RunYear { year }) => {
-            aoc.run_year(
-                *year,
-                &years::Days {
-                    all: true,
-                    days: vec![],
-                },
-            );
-        }
-        None => {
-            aoc.run_all_years();
+    }
+
+    // Optionally print totals.
+    if args().any(|a| a == "--totals") {
+        println!("{BOLD}{YELLOW}⭐ {}{RESET}", 2 * solutions.len());
+        println!("{BOLD}{WHITE}🕓 {} ms{RESET}", duration.as_millis());
+    }
+}
+
+struct Solution {
+    year: u32,
+    day: u32,
+    path: PathBuf,
+    wrapper: fn(String) -> (String, String),
+}
+
+macro_rules! run {
+    ($year:tt $($day:tt),*) => {
+        fn $year() -> Vec<Solution> {
+            vec![$({
+                let year = stringify!($year);
+                let day = stringify!($day);
+                let path = Path::new("input").join(year).join(day).with_extension("txt");
+
+                let wrapper = |data: String| {
+                    use $year::$day::*;
+
+                    let input = parse(&data);
+                    let part1 = part1(&input);
+                    let part2 = part2(&input);
+
+                    (part1.to_string(), part2.to_string())
+                };
+
+                Solution { year: year.unsigned(), day: day.unsigned(), path, wrapper }
+            },)*]
         }
     }
 }
+
+
+run!(year2023
+    day01
+);
